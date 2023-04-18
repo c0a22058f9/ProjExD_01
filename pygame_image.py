1055d6d6ae12ebfebaeb9d03cdea4d8156bac2ec
kudos <c0a22058f9@edu.teu.ac.jp>
2023-04-18 15:38:17 +0900

    2

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01-20230418/fig/pg_bg.jpg")
+    kk_img = pg.transform.flip(pg.image.load("ex01-20230418/fig/3.png"),True,False)
+
+    tmr = 0
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        tmr += 1
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [500,500])
+
+        pg.display.update()
+        clock.tick(100)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
